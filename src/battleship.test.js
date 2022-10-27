f5b65d9f92057fd04b5d8e9406eee4ad7b13d486
Deonte McCoy <deontemccoy19@gmail.com>
2022-10-27 16:30:27 -0500

    Test hitOrMiss function

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,4 +1,4 @@
-import { ship, gameboard } from "./factories.js";
+import { ship, gameboard, hitOrMiss } from "./factories.js";
 
 test("Ship factory function returns an object", () => {
   const testShip = ship(null);
@@ -8,4 +8,16 @@ test("Ship factory function returns an object", () => {
 test("Gameboard factory function returns an object", () => {
   const testBoard = gameboard(null);
   expect(typeof testBoard).toBe("object");
+});
+
+test("hitOrMiss returns if the coordinate was a hit, miss or spot already hit", () => {
+  expect(hitOrMiss(0)).toBe("miss");
+});
+
+test("hitOrMiss returns if the coordinate was a hit, miss or spot already hit", () => {
+  expect(hitOrMiss(1)).toBe("alreadyHit");
+});
+
+test("hitOrMiss returns if the coordinate was a hit, miss or spot already hit", () => {
+  expect(hitOrMiss("boat")).toBe("hit");
 });
