af0084fc4bd55b67c94bb89844845058c04f3753
Deonte McCoy <deontemccoy19@gmail.com>
2022-10-27 13:39:42 -0500

    Test that ship factory function returns an object

---
+++ src/battleship.test.js
@@ -0,0 +1,6 @@
+import { ship } from './factories.js';
+
+test('Ship factory function returns an object', () => {
+    const testShip = ship(null, null, null);
+    expect(typeof(testShip)).toBe('object');
+});
