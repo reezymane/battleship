4ace7099522a8bc48a240ca76d297b0c7976803a
Deonte McCoy <deontemccoy19@gmail.com>
2022-10-27 16:56:04 -0500

    gameboard.receiveAttack() records miss on gameboard

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -26,7 +26,14 @@ test("gameboard.receiveAttack() increases ship hitAmount when coordinate is hit"
   const testShip = ship(2);
   const testBoard = gameboard();
   testBoard.board[1][3] = testShip;
-
   testBoard.receiveAttack([1, 3]);
+
   expect(testShip.hitAmount).toBe(1);
+});
+
+test("gameboard.receiveAttack() records miss on gameboard", () => {
+  const testBoard = gameboard();
+  testBoard.receiveAttack([1, 3]);
+
+  expect(testBoard.board[1][3]).toBe(1);
 });
