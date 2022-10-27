4e42706787bb2dacac8363453b057788ae2d2467
Deonte McCoy <deontemccoy19@gmail.com>
2022-10-27 15:48:33 -0500

    Test gameboard to return object

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,6 +1,11 @@
-import { ship } from "./factories.js";
+import { ship, gameboard } from "./factories.js";
 
 test("Ship factory function returns an object", () => {
-  const testShip = ship(null, null, null);
+  const testShip = ship(null);
   expect(typeof testShip).toBe("object");
+});
+
+test("Gameboard factory function returns an object", () => {
+  const testBoard = gameboard(null);
+  expect(typeof testBoard).toBe("object");
 });
