23ef164e07307aa65dccf4579b332aec5e49dff0
Deonte McCoy <deontemccoy19@gmail.com>
2022-10-28 08:30:12 -0500

    ship.hitAmount increases in ship that spans coordinates

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -37,3 +37,13 @@ test("gameboard.receiveAttack() records miss on gameboard", () => {
 
   expect(testBoard.board[1][3]).toBe(1);
 });
+
+test("ship.hitAmount increases in ship object when spanning coordinates", () => {
+  const testShip = ship(2);
+  const testBoard = gameboard();
+  testBoard.board[1][3] = testShip;
+  testBoard.board[1][4] = testShip;
+  testBoard.receiveAttack([1, 3]);
+  testBoard.receiveAttack([1, 4]);
+  expect(testShip.hitAmount).toBe(2);
+});
