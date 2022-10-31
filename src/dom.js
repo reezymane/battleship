4f2ca9fc9ba25c972e90d63fe1a860d17c84fb42
Deonte McCoy <deontemccoy19@gmail.com>
2022-10-31 13:33:17 -0500

    Add next row with heading and empty cells

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,4 @@
+// Creates the first row of the table and names columns
 const createColumnHeadings = () => {
   const grid = document.querySelector(".p1Grid");
   const row = document.createElement("tr");
@@ -13,8 +14,30 @@ const createColumnHeadings = () => {
   }
 };
 
-const createRows = () => {};
+// Adds next row with heading and empty cells
+const createRows = () => {
+  const grid = document.querySelector(".p1Grid");
 
+  for (let bigI = 0; bigI < 10; bigI++) {
+    const row = document.createElement("tr");
+    const heading = document.createElement("th");
+
+    heading.textContent = bigI + 1;
+
+    grid.appendChild(row);
+    row.appendChild(heading);
+
+    for (let littleI = 0; littleI < 10; littleI++) {
+      const cell = document.createElement("td");
+
+      cell.classList.add(`${bigI},${littleI}`);
+
+      row.appendChild(cell);
+    }
+  }
+};
+
+// Adds table to p1Board div
 const createTable = () => {
   const board = document.querySelector(".p1Board");
   const table = document.createElement("table");
